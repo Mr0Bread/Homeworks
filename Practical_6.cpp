e1b3636fef6fcdb41f8afe4e6e105fcab4dd5896
Vladislavs Zimnikovs <zimnikovvladislav@gmail.com>
2020-04-15 14:55:47 +0300

    "Never forget about newlines!"

--- Practical_6.cpp
+++ Practical_6.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 #define print std::cout
 #define input std::cin
 #define nl std::endl
 #define str std::string
 
+std::string askForFileName();
+
 int main() {
-    str fileName;
+    std::vector<str> stringVector;
+
+    str fileName = askForFileName();
     str line;
+    str smallestCountry;
     std::ifstream file;
-    file.open("countriesSmall.txt");
+    file.open(R"(C:\Users\zimni\CLionProjects\Homeworks\)" + fileName);
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            stringVector.push_back(line);
+            print << line << nl;
+        }
+
+        print << "Smallest country is " << stringVector.at(4) << nl;
+        system("read");
+    } else {
+        print << "Name of file is incorrect!\n";
+    }
 
     file.close();
     getchar();
     return 0;
+}
+
+std::string askForFileName() {
+    str fileName;
+    print << "Enter name of file to open.\nHint: countriesSmall.txt\n> ";
+    input >> fileName;
+    return fileName;
 }
